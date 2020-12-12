2a3c0ab3beec0986f470ee7096886d2883afa4cd
nawaraing <nawaddaing@gmail.com>
2020-12-12 11:55:28 +0900

    Add ft_str_echo_cmp fuction

--- workspace/srcs/branch.c
+++ workspace/srcs/branch.c
@@ -12,8 +12,26 @@
 
 #include "../include/minishell.h"
 
+int		ft_str_echo_cmp(char *command)
+{
+	if (ft_strncmp(command, "echo", 4) == 0)
+		return (1);
+	else if (ft_strncmp(command, "Echo", 4) == 0)
+		return (1);
+	else if (ft_strncmp(command, "\'echo\'", 6) == 0)
+		return (1);
+	else if (ft_strncmp(command, "\"echo\"", 6) == 0)
+		return (1);
+	else if (ft_strncmp(command, "\'Echo\'", 6) == 0)
+		return (1);
+	else if (ft_strncmp(command, "\"Echo\"", 6) == 0)
+		return (1);
+	return (0);
+}
+
 void	command_branch(char *command)
 {
-	if (ft_strncmp(command, "echo", 4))
-		;
+	int			ret = 100;
+	if ((ret = ft_str_echo_cmp(command)))
+		printf("I found echo!\n");
 }
