489170c4f6462a257e66ca993a724593c134305e
nawaraing <nawaddaing@gmail.com>
2020-12-16 23:49:16 +0900

    backup

--- workspace/srcs/branch.c
+++ workspace/srcs/branch.c
@@ -30,6 +30,17 @@ int		ft_isset(const char c, const char *set)
 	return (0);
 }
 
+void		get_str_to_idx(t_word_block *ret, char *line, int i)
+{
+	char		tmp;
+
+	tmp = line[i];
+	line[i] = 0;
+	free(ret->word);
+	ret->word = ft_strdup(line);
+	line[i] = tmp;
+}
+
 void	get_single_quotation(t_word_block *ret, char **ref)
 {
 	char	*line;
@@ -50,9 +61,7 @@ void	get_single_quotation(t_word_block *ret, char **ref)
 		ret->space_has = 1;
 	else if (line[i + 1] == 0)
 		ret->space_has = 2;
-	line[i] = 0;
-	free(ret->word);
-	ret->word = ft_strdup(line);
+	get_str_to_idx(ret, line, i);
 	(*ref) += (i + 1);
 }
 
@@ -80,21 +89,8 @@ void	get_double_quotation(t_word_block *ret, char **ref)
 		ret->space_has = 1;
 	else if (line[i + 1] == 0)
 		ret->space_has = 2;
-	line[i] = 0;
-	free(ret->word);
-	ret->word = ft_strdup(line);
+	get_str_to_idx(ret, line, i);
 	(*ref) += (i + 1);
-}
-
-void		get_str_to_idx(t_word_block *ret, char *line, int i)
-{
-	char		tmp;
-
-	tmp = line[i];
-	line[i] = 0;
-	free(ret->word);
-	ret->word = ft_strdup(line);
-	line[i] = tmp;
 }
 
 int		sep_to_int(char sep, char next)
@@ -107,7 +103,7 @@ int		sep_to_int(char sep, char next)
 		return (REV_REDIR);
 	else if (sep == '|')
 		return (PIPE);
-	else if (sep == ';' || ft_isspace(sep))
+	else if (sep == ';')
 		return (SEMI);
 	return (-1);
 }
@@ -140,7 +136,7 @@ void	get_basic(t_word_block *ret, char **ref)
 		ret->space_has = 1;
 	else if (line[i] == 0)
 		ret->space_has = 2;
-	get_str_to_idx(ret, line, i);						// 
+	get_str_to_idx(ret, line, i);
 	if (line[i] == '>' && i != ft_strlen(line) && line[i + 1] == '>')
 		i++;
 	if (ret->sep == -1)
@@ -168,7 +164,7 @@ t_word_block	get_word(char **ref)
 	char				*line;
 
 	line = *ref;
-	skip_space(&line);
+	skip_space(ref);
 	word_init(&ret);
 	if (line[0] == 0)
 	{
@@ -195,11 +191,17 @@ void	word_free(t_word_block *word)
 	word->space_has = 0;
 }
 
+/*
+* *		두 단어를 조인해줌. 소스는 프리.
+* *		워드는 조인하고, 나머지는 뒤에 붙은 단어의 것을 상속받는다
+* *		사이에 공백이 있는 경우, 공백 하나 추가
+**		param  : 조인할 두 단어
+*/
 void	word_join(t_word_block *dest, t_word_block *srcs)
 {
 	char		*tmp;
 
-	if (dest->space_has)
+	if (dest->space_has == 1)
 	{
 		tmp = dest->word;
 		dest->word = ft_strjoin(dest->word, " ");
@@ -210,132 +212,10 @@ void	word_join(t_word_block *dest, t_word_block *srcs)
 	free(tmp);
 	dest->space_has = srcs->space_has;
 	dest->sep = srcs->sep;
+	dest->quotation = srcs->quotation;
+	dest->fd = srcs->fd;
 	word_free(srcs);
 }
-
-int		cmd_echo_cmp(t_word_block command)
-{
-	if (ft_strlen(command.word) != 4 && ft_strlen(command.word) != 9)
-		return (0);
-	else if (ft_strncmp(command.word, "echo", 4) != 0 && ft_strncmp(command.word, "/bin/echo", 9))
-		return (0);
-	return (1);
-}
-
-void	branch_echo(char **ref)			//	어려워보이니까 나중에 짜자....
-{
-	printf("here is echo part\n");
-}
-
-int		cmd_cd_cmp(t_word_block command)
-{
-	if (ft_strlen(command.word) != 2)
-		return (0);
-	else if (ft_strncmp(command.word, "cd", 2) != 0)
-		return (0);
-	return (1);
-}
-
-void	branch_cd(char **ref)
-{
-	t_word_block	word;
-
-	word = get_word(ref);
-	if (chdir(word.word) != 0)
-		printf("chdir error\t-\tbranch_cd\n");
-	free(word.word);
-}
-
-int		cmd_pwd_cmp(t_word_block command)
-{
-	if (ft_strlen(command.word) != 3 && ft_strlen(command.word) != 8)
-		return (0);
-	else if (ft_strncmp(command.word, "pwd", 3) != 0 && ft_strncmp(command.word, "/bin/pwd", 8) != 0)
-		return (0);
-	return (1);
-}
-
-void	branch_pwd(void)
-{
-	int		size;
-	char	*buf;
-
-	size = (1 << 10);
-	if (getcwd(buf, size) == 0)
-	{
-		write(1, strerror(errno), ft_strlen(strerror(errno)));
-		return ;
-	}
-	write(1, buf, ft_strlen(buf));
-	write(1, "\n", 1);
-}
-
-int		cmd_ls_cmp(t_word_block command)
-{
-	if (ft_strlen(command.word) != 2 && ft_strlen(command.word) != 7)
-		return (0);
-	else if (ft_strncmp(command.word, "ls", 2) != 0 && ft_strncmp(command.word, "/bin/ls", 7) != 0)
-		return (0);
-	return (1);
-}
-
-void	branch_ls(void)
-{
-	DIR				*dir_ptr;
-	struct dirent	*file;
-	char			buf[1 << 10];
-
-	if (getcwd(buf, (1 << 10)) == 0)
-	{
-		write(1, strerror(errno), ft_strlen(strerror(errno)));
-		return ;
-	}
-	if ((dir_ptr = opendir(buf)) == NULL)
-	{
-		write(1, strerror(errno), ft_strlen(strerror(errno)));
-		return ;
-	}
-	while ((file = readdir(dir_ptr)) != NULL)
-	{
-		if (file->d_name[0] == '.')
-			continue ;
-		write(1, file->d_name, ft_strlen(file->d_name));
-		write(1, "\n", 1);
-	}
-}
-
-void	command_branch(char *line)
-{
-	t_word_block		command;
-	t_word_block		next;
-
-	word_init(&command);
-	while (command.space_has == 0)		//	command part
-	{
-		if ((next = get_word(&line)).word == 0){
-			next.word = ft_strdup("");
-			next.space_has = 1;
-		}
-		word_join(&command, &next);
-		word_free(&next);
-	}
-	if (cmd_echo_cmp(command) == 1)
-		branch_echo(&line);
-	else if (cmd_cd_cmp(command) == 1)
-		branch_cd(&line);
-	else if (cmd_pwd_cmp(command) == 1)
-		branch_pwd();
-	else if (cmd_ls_cmp(command) == 1)
-		branch_ls();
-	// else
-		// branch_error();
-}
-
-/*
-* *		under this line is for 'split_separator' function
-**
-**		! means "add fuction in header"
-*/
 
 /*
 * *		스트링을 읽고 어떤 cmd인지 해당 번호를 반환
@@ -383,11 +263,11 @@ void				get_str_and_sep(char **line, char **str, int *sep)
 	t_word_block	string;
 	t_word_block	word;
 
+	skip_space(line);
 	word_init(&string);
-	while ((word = get_word(line)).word)
+	while ((word = get_word(line)).word)		// 무한 루프
 	{
 		word_join(&string, &word);
-		word_free(&word);
 		if (string.sep != -1)
 			break ;
 	}
@@ -412,7 +292,7 @@ void				parse_command(char **line, int *command, char **str, int *sep)
 		{
 			word_join(&cmd, &word);						// ? 뒤에 매개변수 free
 			(*command) = cmd_to_int(cmd.word);			// ? 안에서 free 해주자
-			(*str) = 0;
+			(*str) = ft_strdup("");
 			(*sep) = cmd.sep;
 			break ;
 		}
@@ -435,12 +315,21 @@ t_list				*split_separator(char *line)		//	!
 {
 	t_list			*ret;
 	t_inputs		*content;
+	t_list			*tmp;
 
+	ret = (t_list *)malloc(sizeof(t_list));
 	while (*line)
 	{
 		content = (t_inputs *)malloc(sizeof(t_inputs));
 		parse_command(&line, &(content->command), &(content->str), &(content->sep));
-		ft_lstadd_back(&ret, ft_lstnew(content));
+		printf("==content==\ncommand : %d\nstr : %s\nsep : %d\n", content->command, content->str, content->sep);
+		// printf("before ft_lstadd_back\n");
+		tmp = ft_lstnew(content);
+		if (tmp == 0)
+			printf("tmp is null\n");
+		else
+			ft_lstadd_back(&ret, tmp);
+		// printf("after ft_lstadd_back\n");
 	}
 	return (ret);
 }
